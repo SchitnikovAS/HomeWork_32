8109dc85d618e06082b25e2df9a70cc5dc5688b7
SchitnikovAS <aleksej.baxman@bk.ru>
2024-08-29 09:17:23 +0300

    Add files via upload

--- module_7_3.py
+++ module_7_3.py
@@ -6,9 +6,8 @@ class WordsFinder:
         all_words = {}
         for doc in self.file_names:
             with open(doc, encoding='utf-8') as file:
-                string = file.read()
                 signs = [',', '.', '=', '!', '?', ';', ':', ' - ']
-                text_no_signs = (''.join([c for c in string if c not in signs]))
+                text_no_signs = (''.join([c for c in file if c not in signs]))
                 is_find = 0
                 while is_find >= 0:
                     is_find = text_no_signs.find('\n')
@@ -18,26 +17,21 @@ class WordsFinder:
         return all_words
 
     def find(self, word: str) -> dict:
-        dict_ = self.get_all_words()
-        for key, values in dict_.items():
+        finded_ = {}
+        for key, values in self.get_all_words().items():
             if word.lower() in values:
-                finded_ = {key: values.index(word.lower()) + 1}
-                return finded_
-
+                finded_[key] = values.index(word.lower()) + 1
+        return finded_
 
     def count(self, word: str) -> dict:
-        dict_ = self.get_all_words()
-        count = 0
-        for key, values in dict_.items():
-            if word.lower() in values:
-                for i in values:
-                    if i.lower() == word.lower():
-                        count += 1
-                counted_ = {key: count}
-                return counted_
+        counter_ = {}
+        for key, values in self.get_all_words().items():
+            counter_[key] = values.count(word.lower())
+
+        return counter_
 
 
-finder2 = WordsFinder('test_file.txt')
-print(finder2.get_all_words()) # Все слова
-print(finder2.find('TEXT')) # 3 слово по счёту
-print(finder2.count('teXT')) # 4 слова teXT в тексте всего
+finder2 = WordsFinder('test_file.txt', 'products.txt', 'test.txt')
+print(finder2.get_all_words())  # Все слова
+print(finder2.find('TEXT'))  # 3 слово по счёту
+print(finder2.count('teXT'))  # 4 слова teXT в тексте всего
